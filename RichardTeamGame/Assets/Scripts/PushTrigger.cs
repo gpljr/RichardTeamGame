4290f2d7aa7672dcff243a70960d2736f4d650c1
Brian Pickens <brianpickens53@gmail.com>
2015-02-28 19:19:43 -0800

    PusherTrigger
    Conflicts:
    	RichardTeamGame/Assets/Scenes/camera.unity
    	RichardTeamGame/Assets/Scripts/CameraFollow.cs
    	RichardTeamGame/Assets/Scripts/Movement.cs
    	RichardTeamGame/Assets/Scripts/Movement2.cs

---
+++ RichardTeamGame/Assets/Scripts/PushTrigger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushTrigger : MonoBehaviour {
+
+	public GameObject pusher;
+	private bool pushOn = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (pushOn) {
+			pusher.transform.position += new Vector3(0.1f,0,0);
+				}
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag == "Player1") {
+			pushOn = true;
+
+		} else if (other.gameObject.tag == "Player2") {
+			pushOn = true;
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other){
+		if (other.gameObject.tag == "Player1") {
+			pushOn = false;
+			
+		} else if (other.gameObject.tag == "Player2") {
+			pushOn = false;
+		}
+		}
+}
