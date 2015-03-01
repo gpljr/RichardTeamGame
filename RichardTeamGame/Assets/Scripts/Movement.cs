af87b9050fab70f4b7bd77826dbb5bdec6f9d4a3
Brian Pickens <brianpickens53@gmail.com>
2015-02-28 17:23:25 -0800

    Prototype Moving happy faces around that can push each other

---
+++ RichardTeamGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+
+		public float maxSpeed = 10f;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+	
+				float move = Input.GetAxis ("Horizontal");
+
+				rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+
+				float moveV = Input.GetAxis ("Vertical");
+
+				rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, moveV * maxSpeed);
+
+		}
+}
---
+++ RichardTeamGame/Assets/Scripts/Movement2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement2 : MonoBehaviour {
+
+	public float maxSpeed2 = 10f;
+	public GameObject camera1;
+	public GameObject camera2;
+
+	private bool cSwitch = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+		float move = Input.GetAxis ("Horizontal1");
+		
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed2, rigidbody2D.velocity.y);
+		
+		float moveV = Input.GetAxis ("Vertical1");
+		
+		rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, moveV * maxSpeed2);
+	}
+
+	void Update (){
+		if (!cSwitch && Input.GetKeyDown (KeyCode.Space)) {
+						camera1.SetActive (false);
+						camera2.SetActive (true);
+			cSwitch = true;
+
+				} else if (cSwitch && Input.GetKeyDown (KeyCode.Space)) {
+			camera1.SetActive (true);
+			camera2.SetActive (false);
+			cSwitch = false;
+				}
+
+	}
+
+
+}
