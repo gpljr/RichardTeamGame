59438c638a7bfb37646cc4bc69d131212700f6f7
georgeli <georgeli1025@gmail.com>
2015-03-09 15:46:03 -0700

    split camera demo

---
+++ RichardTeamGame/Assets/Scripts/CameraFollowSplit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowSplit : MonoBehaviour
+{
+		public Transform Player;
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				this.transform.position = new Vector3 (Player.position.x, Player.position.y, -20f);
+
+		}
+}
