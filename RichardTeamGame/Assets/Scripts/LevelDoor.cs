4b83aa02b1cc72007fbf7eb3281c722149c4d260
georgeli <georgeli1025@gmail.com>
2015-03-09 19:22:04 -0700

    reunion level

--- RichardTeamGame/Assets/Scripts/LevelDoor.cs
+++ RichardTeamGame/Assets/Scripts/LevelDoor.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class LevelDoor : MonoBehaviour
 {
-		
+		[SerializeField]
+		private bool
+				isReunion;
 		// Use this for initialization
 		void Start ()
 		{
@@ -19,18 +21,30 @@ public class LevelDoor : MonoBehaviour
 		{
 				if (other.gameObject.tag == "Player1") {
 						Events.g.Raise (new PlayerEnterLevelDoorEvent (isPlayerOne: true));
+						if (isReunion) {
+								other.isTrigger = true;
+						}
 				}
 				if (other.gameObject.tag == "Player2") {
 						Events.g.Raise (new PlayerEnterLevelDoorEvent (isPlayerOne: false));
+						if (isReunion) {
+								other.isTrigger = true;
+						}
 				}
 		}
 		void OnTriggerExit2D (Collider2D other)
 		{
 				if (other.gameObject.tag == "Player1") {
 						Events.g.Raise (new PlayerExitLevelDoorEvent (isPlayerOne: true));
+						if (isReunion) {
+								other.isTrigger = false;
+						}
 				}
 				if (other.gameObject.tag == "Player2") {
 						Events.g.Raise (new PlayerExitLevelDoorEvent (isPlayerOne: false));
+						if (isReunion) {
+								other.isTrigger = false;
+						}
 				}
 		}
 }
