4e3d84f434c67abe1ef8e2f0508ab432ab54e6ef
Brian Pickens <brianpickens53@gmail.com>
2015-03-09 21:40:30 -0700

    separation level finished

---
+++ RichardTeamGame/Assets/Scripts/AutoPusher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoPusher : MonoBehaviour {
+
+	private float startLocation;
+	public float endLocation = 10f;
+	private bool change = true;
+
+	// Use this for initialization
+	void Start () {
+		startLocation = this.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (change && this.transform.position.x < endLocation) {
+			this.rigidbody2D.velocity = new Vector2 (3, rigidbody2D.velocity.y);
+		}
+		if (change && this.transform.position.x > endLocation) {
+			change = false;
+		}
+		if (!change && this.transform.position.x > startLocation) {
+			this.rigidbody2D.velocity = new Vector2 (-3, rigidbody2D.velocity.y);
+		}
+		if (!change && this.transform.position.x < startLocation) {
+			change = true;
+		}
+
+	}
+}
---
+++ RichardTeamGame/Assets/Scripts/AutoPusherUp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoPusherUp : MonoBehaviour {
+
+	private float startLocation;
+	public float endLocation = 10f;
+	private bool change = true;
+	
+	// Use this for initialization
+	void Start () {
+		startLocation = this.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (change && this.transform.position.y < endLocation) {
+			this.rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, 3f);
+		}
+		if (change && this.transform.position.y > endLocation) {
+			change = false;
+		}
+		if (!change && this.transform.position.y > startLocation) {
+			this.rigidbody2D.velocity = new Vector2 (rigidbody2D.velocity.x, -3f);
+		}
+		if (!change && this.transform.position.y < startLocation) {
+			change = true;
+		}
+		
+	}
+}
