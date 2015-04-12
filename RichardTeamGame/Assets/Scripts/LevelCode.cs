514518fb0ad4435a277f5c62052362db957579c0
georgeli <georgeli1025@gmail.com>
2015-04-11 18:00:36 -0700

    fix the music transition

--- RichardTeamGame/Assets/Scripts/LevelCode.cs
+++ RichardTeamGame/Assets/Scripts/LevelCode.cs
@@ -4,33 +4,35 @@ using System.Collections;
 public class LevelCode : MonoBehaviour
 {
 		private bool playerOneEnter, playerTwoEnter;
-		public GameObject bgMusic;
-		public GameObject bgMusicSad;
-		public GameObject bgMusicHappy;
+
+		public float fadeSpeed = 1.5f;          // Speed that the screen fades to and from black.
+
+		//var fadeImage;
+		private bool sceneStarting = true;      // Whether or not the scene is still fading in.
 		// Use this for initialization
 		void Start ()
 		{
-				/*
-				if (Application.loadedLevelName == "StartScreen") {
-						DontDestroyOnLoad (bgMusic);
-				}*/
 				if (Application.loadedLevelName == "sadBegin") {
-						//DontDestroyOnLoad (bgMusicSad);
+						
 						Application.LoadLevel (Application.loadedLevel + 1);
 				}
 				if (Application.loadedLevelName == "happyBegin") {
-						DontDestroyOnLoad (bgMusicHappy);
+						
 						Application.LoadLevel (Application.loadedLevel + 1);
 				}
 				if (Application.loadedLevelName == "beforeReunion1") {
 						Application.LoadLevel (Application.loadedLevel + 1);
 				}
+				//fadeImage = GameObject.FindWithTag ("Fade").GetComponent<Image> ();
 
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
+				if (sceneStarting) {
+						StartScene ();
+				}
 				if (Input.GetKeyDown (KeyCode.Escape)) {
 						Application.Quit ();
 				}
@@ -40,19 +42,19 @@ public class LevelCode : MonoBehaviour
 				}
 
 				if (playerOneEnter && playerTwoEnter) {
-						Application.LoadLevel (Application.loadedLevel + 1);
+						EndScene ();
 				}
 				if (Application.loadedLevelName == "StartScreen") {
 						if (Input.anyKeyDown) {
-								Application.LoadLevel (Application.loadedLevel + 1);
+								EndScene ();
 						}
 				}
 				if (Input.GetKeyDown (KeyCode.N)) {
-						Application.LoadLevel (Application.loadedLevel + 1);
+						EndScene ();
 				}
 				if (Application.loadedLevelName == "reunion2" || Application.loadedLevelName == "reunion3") {
 						if (playerOneEnter) {
-								Application.LoadLevel (Application.loadedLevel + 1);
+								EndScene ();
 						}
 				}
 		}
@@ -81,6 +83,52 @@ public class LevelCode : MonoBehaviour
 						playerOneEnter = false;
 				} else {
 						playerTwoEnter = false;
+				}
+		}
+
+		void FadeToClear ()
+		{
+				// Lerp the colour of the texture between itself and transparent.
+				GUI.color = Color.Lerp (GUI.color, Color.clear, fadeSpeed * Time.deltaTime);
+		}
+	
+	
+		void FadeToBlack ()
+		{
+				// Lerp the colour of the texture between itself and black.
+				GUI.color = Color.Lerp (GUI.color, Color.black, fadeSpeed * Time.deltaTime);
+		}
+	
+	
+		void StartScene ()
+		{
+				// Fade the texture to clear.
+				FadeToClear ();
+		
+				// If the texture is almost clear...
+				if (GUI.color.a <= 0.05f) {
+						// ... set the colour to clear and disable the GUITexture.
+						GUI.color = Color.clear;
+						GUI.enabled = false;
+
+						// The scene is no longer starting.
+						sceneStarting = false;
+				}
+		}
+	
+	
+		public void EndScene ()
+		{
+				// Make sure the texture is enabled.
+				GUI.enabled = true;
+		
+				// Start fading towards black.
+				FadeToBlack ();
+		
+				// If the screen is almost black...
+				if (GUI.color.a >= 0.95f) {
+						// ... reload the level.
+						Application.LoadLevel (Application.loadedLevel + 1);
 				}
 		}
 
