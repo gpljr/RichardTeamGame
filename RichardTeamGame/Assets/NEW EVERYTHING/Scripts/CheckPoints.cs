74fbcd818dbdd51384f26e11025c4f261154a145
Brian Pickens <brianpickens53@gmail.com>
2015-04-19 20:59:59 -0700

    converting levels still

--- RichardTeamGame/Assets/NEW EVERYTHING/Scripts/CheckPoints.cs
+++ RichardTeamGame/Assets/NEW EVERYTHING/Scripts/CheckPoints.cs
@@ -7,6 +7,7 @@ public class CheckPoints : MonoBehaviour {
 	private float temp1y;
 	private float temp2x;
 	private float temp2y;
+	private bool checkPointSaved = false;
 	public Transform player1;
 	public Transform player1Picture;
 	public Transform player2;
@@ -18,23 +19,25 @@ public class CheckPoints : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
-		temp1x = player1.position.x;
-		temp1y = player1.position.y;
-		temp2x = player2.position.x;
-		temp2y = player2.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Return)) {
+			if(checkPointSaved){
 			player1.transform.position = new Vector3 (temp1x,temp1y, 0);
 			player1Picture.transform.position = new Vector3 (temp1x, temp1y, 0);
 			player2.transform.position = new Vector3 (temp2x, temp2y, 0);
 			player2Picture.transform.position = new Vector3 (temp2x, temp2y, 0);
+			}
+			else{
+				Application.LoadLevel (Application.loadedLevel);
+			}
 		}
 
 		if (Check1.GetComponent<CheckPointTrigger> ().isTriggered && Check2.GetComponent<CheckPointTrigger> ().isTriggered2 || Check1.GetComponent<CheckPointTrigger> ().isTriggered2 && Check2.GetComponent<CheckPointTrigger> ().isTriggered) {
 			Debug.Log ("getting info");
+			checkPointSaved = true;
 			temp1x = player1.position.x;
 			temp1y = player1.position.y;
 			temp2x = player2.position.x;
