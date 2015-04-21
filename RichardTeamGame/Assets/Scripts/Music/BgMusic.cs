300965617c487a1eb79990f6866e114949daab87
georgeli <georgeli1025@gmail.com>
2015-04-21 12:56:18 -0700

    change the title and credit scenes

--- RichardTeamGame/Assets/Scripts/Music/BgMusic.cs
+++ RichardTeamGame/Assets/Scripts/Music/BgMusic.cs
@@ -3,57 +3,67 @@ using System.Collections;
 
 public class BgMusic : MonoBehaviour
 {
-		public AudioClip sadMusic;
-		public AudioClip happyMusic;
-		// Use this for initialization
-		void Start ()
-		{
-				DontDestroyOnLoad (this);
-		}
+    public AudioClip sadMusic;
+    public AudioClip happyMusic;
+    public int sadLevel = 8;
+    public int happyLevel = 12;
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
 	
-		// Update is called once per frame
-		void Update ()
-		{
+    // Update is called once per frame
+    void Update()
+    {
 
-				/*if (Application.loadedLevelName == "sadBegin") {
+        /*if (Application.loadedLevelName == "sadBegin") {
 						GameObject.Destroy (this);
 						print ("destroy");
 				}*/
-				//this.transform.position = GameObject.FindGameObjectWithTag ("MainCamera").transform.position;
-		}
-		void OnLevelWasLoaded (int level)
-		{
-				if (level == 9) {
-						while (gameObject.GetComponent<AudioSource> ().volume > 0) {
-								gameObject.GetComponent<AudioSource> ().volume -= 0.1f * Time.deltaTime;
-								print (gameObject.GetComponent<AudioSource> ().volume);
+        //this.transform.position = GameObject.FindGameObjectWithTag ("MainCamera").transform.position;
+    }
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == sadLevel)
+        {
+            while (gameObject.GetComponent<AudioSource> ().volume > 0)
+            {
+                gameObject.GetComponent<AudioSource>().volume -= 0.1f * Time.deltaTime;
+                print(gameObject.GetComponent<AudioSource>().volume);
 						
-								if (gameObject.GetComponent<AudioSource> ().volume < 0.1f) {
-										gameObject.GetComponent<AudioSource> ().clip = sadMusic;
-										gameObject.GetComponent<AudioSource> ().Play ();
-										break;
-								}
+                if (gameObject.GetComponent<AudioSource>().volume < 0.1f)
+                {
+                    gameObject.GetComponent<AudioSource>().clip = sadMusic;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    break;
+                }
 
-						}
-						while (gameObject.GetComponent<AudioSource> ().volume < 1) {
-								gameObject.GetComponent<AudioSource> ().volume += 0.1f * Time.deltaTime;
-						}
-				} else if (level == 13) {
-						while (gameObject.GetComponent<AudioSource> ().volume > 0) {
-								gameObject.GetComponent<AudioSource> ().volume -= 0.1f * Time.deltaTime;
-								print (gameObject.GetComponent<AudioSource> ().volume);
+            }
+            while (gameObject.GetComponent<AudioSource> ().volume < 1)
+            {
+                gameObject.GetComponent<AudioSource>().volume += 0.1f * Time.deltaTime;
+            }
+        } else if (level == happyLevel)
+        {
+            while (gameObject.GetComponent<AudioSource> ().volume > 0)
+            {
+                gameObject.GetComponent<AudioSource>().volume -= 0.1f * Time.deltaTime;
+                print(gameObject.GetComponent<AudioSource>().volume);
 				
-								if (gameObject.GetComponent<AudioSource> ().volume < 0.1f) {
-										gameObject.GetComponent<AudioSource> ().clip = happyMusic;
-										gameObject.GetComponent<AudioSource> ().Play ();
-										break;
-								}
+                if (gameObject.GetComponent<AudioSource>().volume < 0.1f)
+                {
+                    gameObject.GetComponent<AudioSource>().clip = happyMusic;
+                    gameObject.GetComponent<AudioSource>().Play();
+                    break;
+                }
 				
-						}
-						while (gameObject.GetComponent<AudioSource> ().volume < 1) {
-								gameObject.GetComponent<AudioSource> ().volume += 0.1f * Time.deltaTime;
-						}
+            }
+            while (gameObject.GetComponent<AudioSource> ().volume < 1)
+            {
+                gameObject.GetComponent<AudioSource>().volume += 0.1f * Time.deltaTime;
+            }
             
-				}
-		}
+        }
+    }
 }
