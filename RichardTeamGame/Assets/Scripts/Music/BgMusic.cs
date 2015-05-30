1b25605cc4604d1dc12d5fdb0227239e19d836f4
georgeli <georgeli1025@gmail.com>
2015-05-29 21:53:05 -0700

    new art for recording

--- RichardTeamGame/Assets/Scripts/Music/BgMusic.cs
+++ RichardTeamGame/Assets/Scripts/Music/BgMusic.cs
@@ -11,92 +11,92 @@ public class BgMusic : MonoBehaviour
     public AudioClip sadMusic;
     public AudioClip happyMusic;
     // Use this for initialization
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-        print("instance " + instance);
-        if (instance != null)
-        {
-            Destroy(gameObject);
-            print("destroy");
-        }
-        else
-        {
-            instance = this;
-        }
+    // void Awake()
+    // {
+    //     DontDestroyOnLoad(gameObject);
+    //     print("instance " + instance);
+    //     if (instance != null)
+    //     {
+    //         Destroy(gameObject);
+    //         print("destroy");
+    //     }
+    //     else
+    //     {
+    //         instance = this;
+    //     }
         
-    }
+    // }
 
-    void OnEnable()
-    {			
-        Events.g.AddListener<LevelEnteringEvent>(LevelEntered);
-    }
+    // void OnEnable()
+    // {			
+    //     Events.g.AddListener<LevelEnteringEvent>(LevelEntered);
+    // }
 	
-    void OnDisable()
-    {
-        Events.g.AddListener<LevelEnteringEvent>(LevelEntered);
-    }
+    // void OnDisable()
+    // {
+    //     Events.g.AddListener<LevelEnteringEvent>(LevelEntered);
+    // }
 
-    void LevelEntered(LevelEnteringEvent e)
-    {
-        print("level entered");
-        switch (e.levelType)
-        {
-            case LevelType.Starting:
-                _iMusicId = 1;
-                break;
-            case LevelType.Normal:
-                _iMusicId = 1;
-                break;
-            case LevelType.Separated:
-                _iMusicId = 2;
-                break;
-            case LevelType.Combined:
-                _iMusicId = 3;
-                break;
-            default:
-                break;
-        }
-        if (_iCurrentMusicId != _iMusicId)
-        {
-            PlayMusicById(_iMusicId);
-            _iCurrentMusicId = _iMusicId;
-        }
+    // void LevelEntered(LevelEnteringEvent e)
+    // {
+    //     print("level entered");
+    //     switch (e.levelType)
+    //     {
+    //         case LevelType.Starting:
+    //             _iMusicId = 1;
+    //             break;
+    //         case LevelType.Normal:
+    //             _iMusicId = 1;
+    //             break;
+    //         case LevelType.Separated:
+    //             _iMusicId = 2;
+    //             break;
+    //         case LevelType.Combined:
+    //             _iMusicId = 3;
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    //     if (_iCurrentMusicId != _iMusicId)
+    //     {
+    //         PlayMusicById(_iMusicId);
+    //         _iCurrentMusicId = _iMusicId;
+    //     }
 			
-    }
+    // }
 
-    private void PlayMusicById(int musicId)
-    {
-        while (gameObject.GetComponent<AudioSource>().volume > 0)
-        {
-            gameObject.GetComponent<AudioSource>().volume -= 0.5f * Time.deltaTime;
+    // private void PlayMusicById(int musicId)
+    // {
+    //     while (gameObject.GetComponent<AudioSource>().volume > 0)
+    //     {
+    //         gameObject.GetComponent<AudioSource>().volume -= 0.5f * Time.deltaTime;
 						
-            if (gameObject.GetComponent<AudioSource>().volume < 0.1f)
-            {										
-                switch (musicId)
-                {
-                    case 1:
-                        gameObject.GetComponent<AudioSource>().clip = beginMusic;
-                        break;
-                    case 2:
-                        gameObject.GetComponent<AudioSource>().clip = sadMusic;
+    //         if (gameObject.GetComponent<AudioSource>().volume < 0.1f)
+    //         {										
+    //             switch (musicId)
+    //             {
+    //                 case 1:
+    //                     gameObject.GetComponent<AudioSource>().clip = beginMusic;
+    //                     break;
+    //                 case 2:
+    //                     gameObject.GetComponent<AudioSource>().clip = sadMusic;
 
-                        break;
-                    case 3:
-                        gameObject.GetComponent<AudioSource>().clip = happyMusic;
-                        break;
-                    default:
-                        break;
-                }
-                gameObject.GetComponent<AudioSource>().Play();
-                break;
-            }
-        }
-        while (gameObject.GetComponent<AudioSource>().volume < 1)
-        {
-            gameObject.GetComponent<AudioSource>().volume += 0.5f * Time.deltaTime;
-        }
-    }
+    //                     break;
+    //                 case 3:
+    //                     gameObject.GetComponent<AudioSource>().clip = happyMusic;
+    //                     break;
+    //                 default:
+    //                     break;
+    //             }
+    //             gameObject.GetComponent<AudioSource>().Play();
+    //             break;
+    //         }
+    //     }
+    //     while (gameObject.GetComponent<AudioSource>().volume < 1)
+    //     {
+    //         gameObject.GetComponent<AudioSource>().volume += 0.5f * Time.deltaTime;
+    //     }
+    // }
     // Update is called once per frame
     void Update()
     {
--- RichardTeamGame/Assets/Scripts/ShadowController.cs
+++ RichardTeamGame/Assets/Scripts/ShadowController.cs
@@ -13,6 +13,17 @@ public class ShadowController : MonoBehaviour
     private GameObject Char2;
 
     [SerializeField]
+    private GameObject cameraObject1;
+    [SerializeField]
+    private GameObject cameraObject2;
+
+    private Camera camera1;
+    private Camera camera2;
+
+    [SerializeField]
+    private bool _isSingleCamera;
+
+    [SerializeField]
     private Color _bgColor;
 
     [SerializeField]
@@ -21,27 +32,46 @@ public class ShadowController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-	
+        camera1 = cameraObject1.GetComponent<Camera>();
+        camera2 = cameraObject2.GetComponent<Camera>();
     }
-	
+    
     // Update is called once per frame
     void LateUpdate()
     {
-        float aspectRatio = Camera.main.pixelHeight / (float)Camera.main.pixelWidth;
-        //print("Char1.transform.position "+Char1.transform.position);
-        Vector2 vPos1 = Camera.main.WorldToViewportPoint(Char1.transform.position);
-        //print("vPos1 "+vPos1);
-        vPos1.y *= aspectRatio;
-        Vector2 vPos2 = Camera.main.WorldToViewportPoint(Char2.transform.position);
-        vPos2.y *= aspectRatio;
-	
-        _shadowRenderer.material.SetVector("_Position1", vPos1);
-        _shadowRenderer.material.SetVector("_Position2", vPos2);
-        float fDistance = Vector2.Distance(vPos1, vPos2);
-        float radius = _fadeCurve.Evaluate(fDistance);
-        _shadowRenderer.material.SetFloat("_Radius", radius);
+        if (_isSingleCamera)
+        {
+            float aspectRatio = Camera.main.pixelHeight / (float)Camera.main.pixelWidth;
+            //print("Char1.transform.position "+Char1.transform.position);
+            Vector2 vPos1 = Camera.main.WorldToViewportPoint(Char1.transform.position);
+            //print("vPos1 "+vPos1);
+            vPos1.y *= aspectRatio;
+            Vector2 vPos2 = Camera.main.WorldToViewportPoint(Char2.transform.position);
+            vPos2.y *= aspectRatio;
+    
+            _shadowRenderer.material.SetVector("_Position1", vPos1);
+            _shadowRenderer.material.SetVector("_Position2", vPos2);
+            float fDistance = Vector2.Distance(vPos1, vPos2);
+            float radius = _fadeCurve.Evaluate(fDistance);
+            _shadowRenderer.material.SetFloat("_Radius", radius);
 
-        _shadowRenderer.material.SetColor("_Color", _bgColor);
+            _shadowRenderer.material.SetColor("_Color", _bgColor);
+        }
+        else
+        {
+            float aspectRatio1 = camera1.pixelHeight / (float)camera1.pixelWidth;
+            //float aspectRatio2 = camera2.pixelHeight / (float)camera2.pixelWidth;
+            Vector2 vPos1 = camera1.WorldToViewportPoint(Char1.transform.position);
+            vPos1.y *= aspectRatio1;
+            print("vPos1 "+vPos1);
+            _shadowRenderer.material.SetVector("_Position1", vPos1);
+            _shadowRenderer.material.SetVector("_Position2", vPos1);
+            float radius = 0.05f;
+            _shadowRenderer.material.SetFloat("_Radius", radius);
 
+            _shadowRenderer.material.SetColor("_Color", _bgColor);
+            //Vector2 vPos2 = camera2.WorldToViewportPoint(Char2.transform.position);
+            //vPos2.y *= aspectRatio2;
+        }
     }
 }
